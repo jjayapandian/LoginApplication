bc9ea1f5b85403860497f0ae7dd4cf4f1206963a
DharaniMoorthy <itdharanim@gmail.com>
2018-08-14 10:53:46 +0530

    create db jwt and USER table

---
+++ db.sql
@@ -0,0 +1,14 @@
+create database jwt;
+
+use jwt;
+
+CREATE TABLE `USER` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `regdate` date NOT NULL,
+  PRIMARY KEY (`id`)
+);
